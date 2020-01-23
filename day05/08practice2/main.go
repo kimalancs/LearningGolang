63c8ae4a32c5da92767d8f4ee72e17b4c37938ef
kimalan <kimalancs@gmail.com>
2020-01-23 21:58:29 +0800

    day09

---
+++ day05/08practice2/main.go
@@ -0,0 +1,26 @@
+package main
+
+
+/*
+日志库
+使用接口的方式实现一个既可以向终端写日志也可以向文件写日志的简易日志库
+
+日志要分等级
+logger.Trace()
+logger.Debug()
+logger.Warning()
+logger.Info()
+logger.Error()
+
+
+fmt.Fprintln(os.Stdout,"这是一条日志")
+
+fileObj, _ := os.OpenFile("./xxx.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+fmt.Fprintln(fileObj, "这是一条日志")
+*/
+
+
+
+func main() {
+
+}

95f2bee57d7a907062bd55c0e2dc7ab127a3b140
kimalan <kimalancs@gmail.com>
2020-01-29 17:35:29 +0800

    reflect_practice_04

---
+++ day06/reflect_practice2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// ini配置文件解析器
+// 使用反射从config.ini文件中获取字段信息传入struct结构体
+
+// MysqlConfig MySQL配置结构体
+type MysqlConfig struct {
+	Address  string `ini:"address"`
+	Port     int    `ini:"port"`
+	Username string `ini:"username"`
+	Password string `ini:"password"`
+}
+
+func loadIni(v interface{}) {
+
+}
+
+func main() {
+	var mc MysqlConfig
+	loadIni(&mc)
+	fmt.Println(mc.Address, mc.Port, mc.Username, mc.Password)
+}

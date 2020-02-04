5006e12deb129c17ebc8c17be5daf3f6c0b2db49
kimalan <kimalancs@gmail.com>
2020-02-04 22:13:33 +0800

    net/http server

---
+++ day09/unitTesting1/main.go
@@ -0,0 +1,15 @@
+package main
+
+// TDD test driven development
+// 单元测试
+// go test命令是一个按照一定约定和组织的测试代码的驱动程序
+// 在包目录内，所有以_test.go为后缀名的源代码文件都是go test测试的一部分，不会被go build编译到最终的可执行文件中
+// 
+// 
+
+
+
+func main() {
+
+
+}
